901f675abe127d554e9b88c632327de56ce9d3e6
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-13 09:33:53 -0600

    Create all tests for iteration 3

--- test/house_test.rb
+++ test/house_test.rb
@@ -33,4 +33,57 @@ class HouseTest < Minitest::Test
     house.add_room(room_2)
     assert_equal [room_1, room_2], house.rooms
   end
+
+  def test_it_is_only_above_market_price_if_over_500000
+    skip
+    house = House.new("$400000", "123 sugar lane")
+    house_2 = House.new("$500000", "234 sugar lane")
+    refute house.above_market_average?
+    assert house_2.above_market_average?
+  end
+
+  def test_it_can_sort_room_by_category
+    skip
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+    assert_equal [], house.rooms_from_category(:bedroom)
+    assert_equal [], house.rooms_from_category(:basement)
+    assert_equal [], house.rooms_from_category(:living_room)
+  end
+
+  def test_it_can_calculate_total_house_area
+    skip
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+    assert_equal 1900, house.area
+  end
+
+  def test_it_can_give_house_details
+    skip
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+    expected = {"price => 400000", "address" => "123 sugar lane"}
+    assert_equal expected, house.details
+  end
 end
